87358b28cc6eb861435a4db4026e081cf928f3e1
Dason <luhuixian1953@gmail.com>
2026-01-16 18:54:57 -0500

    Works.

--- Circle.py
+++ Circle.py
@@ -28,20 +28,37 @@ class Circle():
         self.y = self.originy + 50
         self.vely = 1
         self.velx = 1
+        self.track = 0
+        self.dir = True
+
+    def math(self):
+        #y^2 + x^2 = r^2
+        y = self.y**2
+        x = self.track**2
+        rr = self.rr**2
+        if self.dir:
+            self.y = (rr - x)**0.5 + self.originy
+        else:
+            self.y = -(rr - x)**0.5 + self.originy
 
     def move(self):
+
         if (self.x >= self.originx + self.rr) or (self.x <= self.originx - self.rr):
             self.velx *= -1
-        if (self.y >= self.originy + self.rr) or (self.y <= self.originy - self.rr):
-            self.vely *= -1
+            self.dir = not self.dir
+        # if (self.y >= self.originy + self.rr) or (self.y <= self.originy - self.rr*2):
+        #     self.vely *= -1
+
+        self.track += self.velx
         self.x += self.velx
         self.y += self.vely
 
     def draw(self):
+        pygame.draw.circle(screen,WHITE,(self.originx,self.originy),10) #origin circle
         pygame.draw.circle(screen,RED,(self.x,self.y),self.r)
 
 
-Round = Circle(25,100)
+Round = Circle(20,100)
  
 running = True
 while running:
@@ -52,6 +69,7 @@ while running:
     #clear the screen
     screen.fill(BLACK)
 
+    Round.math()
     Round.move()
     Round.draw()
 
